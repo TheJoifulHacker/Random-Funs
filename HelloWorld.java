9225621452d8fc9e55455cfa191d05f81721f091
TheJoifulHacker <zjtheprofessional@gmail.com>
2025-12-30 11:30:47 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Author: Zachary Joy
+// Date: 12.30.2025
+// Purpose: Hello World!
+
+public class LabTemplate {
+	public static void main (String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
